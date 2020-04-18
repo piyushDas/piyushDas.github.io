38c60aab9d3017abcd4e8215ca33bb5038e58ba5
Piyush Das <piyush.das@cleartrip.com>
2020-04-18 15:53:05 +0530

    adding the tic-tac-toe link

---
+++ tic-tac-toe/precache-manifest.5b10b0cfb44b050e94c9bd0fad77d0de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dafd5512cc0d06ee8cf13bce7300fc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1d539c66efe5f52895c",
+    "url": "/static/css/main.bf0be001.chunk.css"
+  },
+  {
+    "revision": "88eeed1641bcf4deb773",
+    "url": "/static/js/2.c1a426dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1a426dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d539c66efe5f52895c",
+    "url": "/static/js/main.96d2a71e.chunk.js"
+  },
+  {
+    "revision": "67d72a76888994cd830e",
+    "url": "/static/js/runtime-main.3fe58472.js"
+  }
+]);
