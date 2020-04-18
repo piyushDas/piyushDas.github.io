6df18de0bb479ed2fab26eac782ef4ce0981ee4d
Piyush Das <piyush.das@cleartrip.com>
2020-04-18 21:23:24 +0530

    Circle select game hoisted

---
+++ circle-game/precache-manifest.7ee51b57f4fd52acda60b621daf80908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a5f1fb2cb5797c5a85a752904a1e51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53f8dc18432573f68549",
+    "url": "/static/css/main.9556ddd1.chunk.css"
+  },
+  {
+    "revision": "2600df1da3d450d52613",
+    "url": "/static/js/2.293e8fa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.293e8fa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f8dc18432573f68549",
+    "url": "/static/js/main.821b0838.chunk.js"
+  },
+  {
+    "revision": "67d72a76888994cd830e",
+    "url": "/static/js/runtime-main.3fe58472.js"
+  }
+]);
